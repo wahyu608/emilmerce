ce18261c9280b8b54a88785a0e170d5e0293f404
Emillia Cantella <emilliacantella@Emillias-MacBook-Air.local>
2026-01-06 11:49:54 +0800

    hello emilmerce

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{flex: 1}}>
+      <View 
+        style={{
+          backgroundColor:'rgba(233, 233, 233, 1)',
+          padding: 16,          
+        }}>
+        <Text
+          style={{
+            color: '#000'
+          }}> hello emil</Text>
+      </View>
+      <View 
+        style={{
+          flex: 1,
+          backgroundColor:'rgba(255, 255, 255, 1)',
+          padding: 16,          
+        }}>
+        <Text
+          style={{
+            color: '#000'
+          }}> selamat datang di emilmerce</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
